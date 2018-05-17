ea5be7260990ea9047fb432a2427e66a48a83b55
priyanga24 <34644984+priyanga24@users.noreply.github.com>
2018-05-17 19:50:32 +0530

    Create count.c

---
+++ code katta/count.c
@@ -0,0 +1,28 @@
+/*package whatever //do not write package name here */
+#include<stdio.h>
+int main()
+{
+    int a[100],i,j,k,n,count[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        count[a[i]]=0;
+    }
+    for(i=0;i<n;i++)
+    {
+        count[a[i]]++;
+    }
+    int max=-1;
+    for(i=0;i<n;i++)
+    {
+        if(count[a[i]]>max)
+        {
+            max=count[a[i]];
+            printf("%d",a[i]);
+        }
+    }
+}
